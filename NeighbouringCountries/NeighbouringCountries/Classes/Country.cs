d9e616a4eebd61a9fa5a7e51204b6e4a7f4a2076
olotolo <olotolo@web.de>
2023-02-21 22:06:56 +0100

    Input box

---
+++ NeighbouringCountries/NeighbouringCountries/Classes/Country.cs
@@ -0,0 +1,11 @@
+﻿public class Country
+{
+    public string GivenCountry { get; set; } = string.Empty;
+    public List<string> NeighbouringCountries { get; set; } = new List<string>();
+
+    public Country(string givenCountry, List<string> neighbouringCountries)
+    {
+        GivenCountry = givenCountry;
+        NeighbouringCountries = neighbouringCountries;
+    }
+}
