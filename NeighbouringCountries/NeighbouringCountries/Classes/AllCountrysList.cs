46c5d50115235fe10f77da9e4f2392b9eaeb331c
olotolo <olotolo@web.de>
2023-02-22 15:29:43 +0100

    Implemented most countries

--- NeighbouringCountries/NeighbouringCountries/Classes/AllCountrysList.cs
+++ NeighbouringCountries/NeighbouringCountries/Classes/AllCountrysList.cs
@@ -1,17 +1,231 @@
-﻿ public class AllCountrysList
+﻿public class AllCountrysList
 {
-    public List<Country> Countrys = new List<Country>
+
+    public Dictionary<string, List<string>> countryBordersGerman = new Dictionary<string, List<string>>()
     {
-        new Country("Deutschland", new List<string>{"Dänemark", "Polen", "Tschechien", "Österreich", "Schweiz", "Frankreich", "Luxemburg", "Belgien", "Niederlande" }),
-        new Country("Norwegen", new List<string>{"Schweden", "Finnland", "Russland"}),
-        new Country("Schweden", new List<string>{"Norwegen", "Finnland"}),
-        new Country("Finnland", new List<string>{"Norwegen", "Schweden", "Russland"}),
-        new Country("Albanien", new List<string>{"Griechenland", "Kosovo", "Mazedonien", "Montenegro", "Serbien"}),
 
+        //Europe
 
+        { "Albanien", new List<string>{"Montenegro", "Kosovo", "Nordmazedonien", "Griechenland"}},
+        { "Andorra", new List<string>{"Spanien", "Frankreich"}},
+        { "Österreich", new List<string>{"Deutschland", "Tschechien", "Slowakei", "Ungarn", "Slowenien", "Italien", "Schweiz", "Liechtenstein"}},
+        { "Belarus", new List<string>{"Polen", "Litauen", "Lettland", "Russland", "Ukraine"}},
+        { "Belgien", new List<string>{"Niederlande", "Deutschland", "Luxemburg", "Frankreich"}},
+        { "Bosnien und Herzegowina", new List<string>{"Montenegro", "Serbien", "Kroatien"}},
+        { "Bulgarien", new List<string>{"Rumänien", "Serbien", "Mazedonien", "Griechenland", "Türkei"}},
+        { "Kroatien", new List<string>{"Slowenien", "Ungarn", "Serbien", "Bosnien und Herzegowina"}},
+        { "Zypern", new List<string>{"none"}},
+        { "Tschechien", new List<string>{"Deutschland", "Polen", "Slowakei", "Österreich"}},
+        { "Dänemark", new List<string>{"Deutschland"}},
+        { "Estland", new List<string>{"Russland", "Lettland"}},
+        { "Finnland", new List<string>{"Russland", "Norwegen", "Schweden"}},
+        { "Frankreich", new List<string>{"Belgien", "Luxemburg", "Deutschland", "Schweiz", "Italien", "Spanien", "Andorra", "Monaco"}},
+        { "Deutschland", new List<string>{"Dänemark", "Polen", "Tschechien", "Österreich", "Schweiz", "Frankreich", "Niederlande", "Belgien", "Luxemburg"}},
+        { "Griechenland", new List<string>{"Albanien", "Mazedonien", "Bulgarien", "Türkei"}},
+        { "Ungarn", new List<string>{"Österreich", "Slowakei", "Ukraine", "Rumänien", "Serbien", "Kroatien"}},
+        { "Island", new List<string>{"none"}},
+        { "Irland", new List<string>{"Vereinigtes Königreich"}},
+        { "Italien", new List<string>{"Schweiz", "Österreich", "Slowenien", "Frankreich", "Vatikanstadt", "San Marino"}},
+        { "Kosovo", new List<string>{"Albanien", "Montenegro", "Serbien", "Mazedonien"}},
+        { "Lettland", new List<string>{"Estland", "Litauen", "Russland"}},
+        { "Liechtenstein", new List<string>{"Schweiz", "Österreich"}},
+        { "Litauen", new List<string>{"Lettland", "Polen", "Russland", "Weißrussland"}},
+        { "Luxemburg", new List<string>{"Belgien", "Deutschland", "Frankreich"}},
+        { "Malta", new List<string>{"none"}},
+        { "Moldawien", new List<string>{"Rumänien", "Ukraine"}},
+        { "Monaco", new List<string>{"Frankreich"}},
+        { "Montenegro", new List<string>{"Bosnien und Herzegowina", "Serbien", "Albanien", "Kosovo"}},
+        { "Niederlande", new List<string>{"Deutschland", "Belgien"}},
+        { "Nordmazedonien", new List<string>{"Serbien", "Kosovo", "Albanien", "Griechenland", "Bulgarien"}},
+        { "Norwegen", new List<string>{"Schweden", "Finnland", "Russland"}},
+        { "Polen", new List<string>{"Deutschland", "Tschechien", "Slowakei", "Ukraine", "Weißrussland", "Litauen"}},
+        { "Portugal", new List<string>{"Spanien"}},
+        { "Rumänien", new List<string>{"Ukraine", "Moldawien", "Bulgarien", "Serbien", "Ungarn"}},
+        { "Russland", new List<string>{"Norwegen", "Finnland", "Estland", "Lettland", "Litauen", "Polen", "Weißrussland", "Ukraine", "Georgien", "Aserbaidschan", "Kasachstan", "Mongolei"}},
+        { "San Marino", new List<string>{"Italien"}},
+        { "Serbien", new List<string>{"Ungarn", "Rumänien", "Bulgarien", "Mazedonien", "Kosovo", "Montenegro", "Bosnien und Herzegowina"}},
+        { "Slowakei", new List<string>{"Polen", "Tschechien", "Ungarn", "Österreich"}},
+        { "Slowenien", new List<string>{"Österreich", "Italien", "Ungarn", "Kroatien"}},
+        { "Spanien", new List<string>{"Portugal", "Frankreich", "Andorra"}},
+        { "Schweden", new List<string>{"Norwegen", "Finnland"}},
+        { "Schweiz", new List<string>{"Deutschland", "Österreich", "Liechtenstein", "Italien", "Frankreich"}},
+        { "Ukraine", new List<string>{"Weißrussland", "Russland", "Polen", "Slowakei", "Ungarn", "Rumänien", "Moldawien"}},
+        { "Vatikanstadt", new List<string>{"Italien"}},
+        { "Vereinigtes Königreich", new List<string>{"Irland"}},
+        { "Weißrussland", new List<string>{"Polen", "Litauen", "Lettland", "Russland", "Ukraine"}},
+        { "Königreich", new List<string>{"Belgien", "Dänemark", "Deutschland", "Frankreich", "Gibraltar", "Irland", "Italien", "Luxemburg", "Niederlande", "Portugal", "Spanien", "Schweden"}},
+
+        //Asia
+
+        { "Afghanistan", new List<string>{"Iran", "Pakistan", "Tadschikistan", "Turkmenistan", "Usbekistan"}},
+        { "Bahrain", new List<string>{"Saudi-Arabien", "Katar"}},
+        { "Bangladesch", new List<string>{"Indien", "Myanmar"}},
+        { "Bhutan", new List<string>{"China", "Indien"}},
+        { "Brunei", new List<string>{"Malaysia"}},
+        { "China", new List<string>{"Afghanistan", "Bhutan", "Indien", "Kasachstan", "Kirgisistan", "Laos", "Mongolei", "Myanmar", "Nepal", "Nordkorea", "Pakistan", "Russland", "Tadschikistan", "Vietnam"}},
+        { "Indien", new List<string>{"Bangladesch", "Bhutan", "China", "Myanmar", "Nepal", "Pakistan"}},
+        { "Indonesien", new List<string>{"Osttimor", "Papua-Neuguinea", "Malaysia"}},
+        { "Irak", new List<string>{"Iran", "Jordanien", "Kuwait", "Saudi-Arabien", "Syrien", "Türkei"}},
+        { "Iran", new List<string>{"Afghanistan", "Armenien", "Aserbaidschan", "Irak", "Pakistan", "Türkei", "Turkmenistan"}},
+        { "Israel", new List<string>{"Ägypten", "Jordanien", "Libanon", "Syrien"}},
+        { "Japan", new List<string>{"China", "Nordkorea", "Südkorea", "Russland"}},
+        { "Jemen", new List<string>{"Saudi-Arabien", "Oman"}},
+        { "Jordanien", new List<string>{"Irak", "Israel", "Saudi-Arabien", "Syrien"}},
+        { "Kambodscha", new List<string>{"Laos", "Thailand", "Vietnam"}},
+        { "Kasachstan", new List<string>{"China", "Kirgisistan", "Russland", "Turkmenistan", "Usbekistan"}},
+        { "Katar", new List<string>{"Bahrain", "Saudi-Arabien"}},
+        { "Kirgisistan", new List<string>{"China", "Kasachstan", "Tadschikistan", "Usbekistan"}},
+        { "Kuwait", new List<string>{"Irak", "Saudi-Arabien"}},
+        { "Laos", new List<string>{"China", "Kambodscha", "Myanmar", "Thailand", "Vietnam"}},
+        { "Libanon", new List<string>{"Israel", "Syrien"}},
+        { "Malaysia", new List<string>{"Brunei", "Indonesien", "Thailand", "Singapur"}},
+        { "Malediven", new List<string>{"none"}},
+        { "Mongolei", new List<string>{"China", "Russland"}},
+        { "Myanmar", new List<string>{"Bangladesch", "China", "Indien", "Laos", "Thailand"}},
+        { "Nepal", new List<string>{"China", "Indien"}},
+        { "Nordkorea", new List<string>{"China", "Russland", "Südkorea"}},
+        { "Oman", new List<string>{"Jemen", "Saudi-Arabien", "Vereinigte Arabische Emirate"}},
+        { "Pakistan", new List<string>{"Afghanistan", "China", "Indien", "Iran"}},
+        { "Philippinen", new List<string>{"none"}},
+        { "Saudi-Arabien", new List<string>{"Irak", "Jemen", "Jordanien", "Kuwait", "Oman", "Katar", "Vereinigte Arabische Emirate"}},
+        { "Singapur", new List<string>{"Malaysia"}},
+        { "Sri Lanka", new List<string>{"none"}},
+        { "Südkorea", new List<string>{"Nordkorea"}},
+        { "Syrien", new List<string>{"Irak", "Israel", "Jordanien", "Libanon", "Türkei"}},
+        { "Tadschikistan", new List<string>{"Afghanistan", "China", "Kirgisistan", "Usbekistan"}},
+        { "Thailand", new List<string>{"Kambodscha", "Laos", "Malaysia", "Myanmar"}},
+        { "Turkmenistan", new List<string>{"Afghanistan", "Iran", "Kasachstan", "Usbekistan"}},
+        { "Usbekistan", new List<string>{"Afghanistan", "Kasachstan", "Kirgisistan", "Tadschikistan", "Turkmenistan"}},
+        { "Vereinigte Arabische Emirate", new List<string>{"Oman", "Saudi-Arabien"}},
+        { "Vietnam", new List<string>{"China", "Kambodscha", "Laos"}},
+
+        // Ozeanien
         /*
-        new Country("Norwegen", new List<string>{}),
+        { "Australien", new List<string>{"Indonesien", "Neuseeland"}},
+        { "Fidschi", new List<string>{"none"}},
+        { "Kiribati", new List<string>{"none"}},
+        { "Marshallinseln", new List<string>{"none"}},
+        { "Mikronesien", new List<string>{"none"}},
+        { "Nauru", new List<string>{"none"}},
+        { "Neuseeland", new List<string>{"Australien"}},
+        { "Palau", new List<string>{"none"}},
+        { "Papua-Neuguinea", new List<string>{"Indonesien"}},
+        { "Salomonen", new List<string>{"none"}},
+        { "Samoa", new List<string>{"none"}},
+        { "Tonga", new List<string>{"none"}},
+        { "Tuvalu", new List<string>{"none"}},
+        { "Vanuatu", new List<string>{"none"}}
         */
+        // Nordamerika
+
+
+
+        // Südamerika
+
+
+        // Afrika
+
+        {"Ägypten", new List<string>{"Libyen", "Sudan", "Israel", "Gaza-Streifen"}},
+        {"Algerien", new List<string>{"Tunesien", "Libyen", "Mali", "Niger", "Westsahara", "Marokko", "Mauretanien"}},
+        {"Angola", new List<string>{"Kongo", "Namibia", "Sambia"}},
+        {"Äquatorialguinea", new List<string>{"Kamerun", "Gabun"}},
+        {"Äthiopien", new List<string>{"Eritrea", "Dschibuti", "Somalia", "Kenia", "Südsudan"}},
+        {"Benin", new List<string>{"Burkina Faso", "Niger", "Nigeria", "Togo"}},
+        {"Botswana", new List<string>{"Namibia", "Südafrika", "Simbabwe"}},
+        {"Burkina Faso", new List<string>{"Benin", "Elfenbeinküste", "Ghana", "Mali", "Niger", "Togo"}},
+        {"Burundi", new List<string>{"Ruanda", "Tansania", "Kongo"}},
+        {"Elfenbeinküste", new List<string>{"Burkina Faso", "Ghana", "Guinea", "Liberia", "Mali"}},
+        {"Dschibuti", new List<string>{"Eritrea", "Äthiopien", "Somalia"}},
+        {"Eritrea", new List<string>{"Äthiopien", "Dschibuti", "Sudan"}},
+        {"Gabun", new List<string>{"Äquatorialguinea", "Kamerun", "Kongo"}},
+        {"Gambia", new List<string>{"Senegal"}},
+        {"Ghana", new List<string>{"Burkina Faso", "Elfenbeinküste", "Togo"}},
+        {"Guinea", new List<string>{"Guinea-Bissau", "Liberia", "Mali", "Senegal", "Sierra Leone"}},
+        {"Guinea-Bissau", new List<string>{"Guinea", "Senegal"}},
+        {"Kamerun", new List<string>{"Zentralafrikanische Republik", "Tschad", "Kongo", "Äquatorialguinea", "Gabun", "Nigeria"}},
+        {"Kenia", new List<string>{"Äthiopien", "Somalia", "Südsudan", "Tansania", "Uganda"}},
+        {"Komoren", new List<string>{"Mosambik", "Madagaskar"}},
+        {"Kongo", new List<string>{"Kongo (Dem. Rep.)", "Zentralafrikanische Republik", "Angola", "Gabun", "Kamerun"}},
+        {"Kongo (Dem. Rep.)", new List<string>{"Kongo", "Zentralafrikanische Republik", "Südsudan", "Uganda", "Ruanda", "Burundi", "Tansania", "Sambia", "Angola"}},
+        {"Lesotho", new List<string>{"Südafrika"}},
+        {"Liberia", new List<string>{"Elfenbeinküste", "Guinea", "Sierra Leone"}},
+
+
+
+
+
+
     };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Dictionary<string, List<string>> countryBordersEuropeEnglish = new Dictionary<string, List<string>>()
+    {
+        {"Albania", new List<string>{"Greece", "North Macedonia", "Kosovo", "Montenegro"}},
+        {"Andorra", new List<string>{"France", "Spain"}},
+        {"Austria", new List<string>{"Germany", "Czech Republic", "Slovakia", "Hungary", "Slovenia", "Italy", "Switzerland", "Liechtenstein"}},
+        {"Belarus", new List<string>{"Russia", "Latvia", "Lithuania", "Poland", "Ukraine"}},
+        {"Belgium", new List<string>{"Netherlands", "Germany", "Luxembourg", "France"}},
+        {"Bosnia and Herzegovina", new List<string>{"Croatia", "Serbia", "Montenegro"}},
+        {"Bulgaria", new List<string>{"Romania", "Serbia", "North Macedonia", "Greece", "Turkey"}},
+        {"Croatia", new List<string>{"Slovenia", "Hungary", "Serbia", "Bosnia and Herzegovina", "Montenegro"}},
+        {"Cyprus", new List<string>{"Turkish Republic of Northern Cyprus"}},
+        {"Czech Republic", new List<string>{"Germany", "Poland", "Slovakia", "Austria"}},
+        {"Denmark", new List<string>{"Germany"}},
+        {"Estonia", new List<string>{"Russia", "Latvia"}},
+        {"Finland", new List<string>{"Russia", "Sweden", "Norway"}},
+        {"France", new List<string>{"Belgium", "Luxembourg", "Germany", "Switzerland", "Italy", "Spain", "Andorra", "Monaco"}},
+        {"Germany", new List<string>{"Denmark", "Netherlands", "Belgium", "Luxembourg", "France", "Switzerland", "Austria", "Czech Republic", "Poland"}},
+        {"Greece", new List<string>{"Albania", "North Macedonia", "Bulgaria", "Turkey"}},
+        {"Hungary", new List<string>{"Slovakia", "Ukraine", "Romania", "Serbia", "Croatia", "Austria", "Slovenia"}},
+        {"Iceland", new List<string>{"No land borders"}},
+        {"Ireland", new List<string>{"No land borders"}},
+        {"Italy", new List<string>{"France", "Switzerland", "Austria", "Slovenia", "Vatican City", "San Marino"}},
+        {"Kosovo", new List<string>{"Albania", "Montenegro", "Serbia", "North Macedonia"}},
+        {"Latvia", new List<string>{"Estonia", "Russia", "Belarus", "Lithuania"}},
+        {"Liechtenstein", new List<string>{"Switzerland", "Austria"}},
+        {"Lithuania", new List<string>{"Latvia", "Belarus", "Poland", "Russia"}},
+        {"Luxembourg", new List<string>{"Belgium", "Germany", "France"}},
+        {"Malta", new List<string>{"No land borders"}},
+        {"Moldova", new List<string>{"Romania", "Ukraine"}},
+        {"Monaco", new List<string>{"France"}},
+        {"Montenegro", new List<string>{"Bosnia and Herzegovina", "Serbia", "Albania", "Kosovo"}},
+        {"Netherlands", new List<string>{"Belgium", "Germany"}},
+        {"North Macedonia", new List<string>{"Kosovo", "Serbia", "Bulgaria", "Greece", "Albania"}},
+        {"Norway", new List<string>{"Sweden", "Finland", "Russia"}},
+        {"Poland", new List<string>{"Germany", "Czech Republic", "Slovakia", "Ukraine", "Belarus", "Lithuania"}},
+        {"Portugal", new List<string>{"Spain"}},
+        {"Romania", new List<string>{"Hungary", "Ukraine", "Moldova", "Bulgaria", "Serbia"}},
+        {"Russia", new List<string>{"Norway", "Finland", "Estonia", "Latvia", "Lithuania", "Poland", "Belarus", "Ukraine", "Georgia", "Azerbaijan", "Kazakhstan", "Mongolia", "China", "North Korea"}},
+        {"San Marino", new List<string>{"Italy"}},
+        {"Serbia", new List<string>{"Hungary", "Romania", "Bulgaria", "North Macedonia", "Kosovo", "Bosnia and Herzegovina", "Montenegro"}},
+        {"Slovakia", new List<string>{"Czech Republic", "Poland", "Ukraine", "Hungary", "Austria"}},
+        {"Slovenia", new List<string>{"Italy", "Austria", "Hungary", "Croatia"}},
+        {"Spain", new List<string>{"Portugal", "France", "Andorra"}},
+        {"Sweden", new List<string>{"Norway", "Finland"}},
+        {"Switzerland", new List<string>{"Germany", "Austria", "Liechtenstein", "Italy", "France"}},
+        {"Turkey", new List<string>{"Greece", "Bulgaria", "Georgia", "Armenia", "Azerbaijan", "Iran", "Iraq", "Syria"}},
+        {"Ukraine", new List<string>{"Poland", "Slovakia", "Hungary", "Romania", "Moldova", "Belarus", "Russia"}},
+        {"United Kingdom", new List<string>{"Ireland"}},
+        {"Vatican City", new List<string>{"Italy"}}
+    };
+
+
+
 }
+
 
